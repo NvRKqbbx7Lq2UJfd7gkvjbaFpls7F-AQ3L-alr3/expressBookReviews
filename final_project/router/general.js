9dab666e89862aea86bbe0b65d822a5ee6171e21
NvRKqbbx7Lq2UJfd7gkvjbaFpls7F-AQ3L-alr3 <191168132+NvRKqbbx7Lq2UJfd7gkvjbaFpls7F-AQ3L-alr3@users.noreply.github.com>
2024-12-21 19:53:37 -0500

    task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,7 +18,6 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-	// Extract the email parameter from the request URL
 	const isbn = req.params.isbn;
 	console.log(`looking for isbn: ${isbn}`);
 	return res.json(books[isbn]);
@@ -51,7 +50,9 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
 	//Write your code here
-	return res.status(300).json({message: "Yet to be implemented"});
+	const isbn = req.params.isbn;
+	console.log(`looking for review of isbn: ${isbn}`);
+	return res.json(books[isbn].review);
 });
 
 module.exports.general = public_users;
