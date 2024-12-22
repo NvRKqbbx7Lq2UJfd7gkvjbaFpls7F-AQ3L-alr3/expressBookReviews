633f2dbd567326393e1a075f85d3689ddede3e00
NvRKqbbx7Lq2UJfd7gkvjbaFpls7F-AQ3L-alr3 <191168132+NvRKqbbx7Lq2UJfd7gkvjbaFpls7F-AQ3L-alr3@users.noreply.github.com>
2024-12-21 19:50:25 -0500

    task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,27 +21,37 @@ public_users.get('/isbn/:isbn',function (req, res) {
 	// Extract the email parameter from the request URL
 	const isbn = req.params.isbn;
 	console.log(`looking for isbn: ${isbn}`);
-	//let filtered_books = books.filter((book) => book.isbn === isbn);
 	return res.json(books[isbn]);
 });
 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-	// Retrieve the email parameter from the request URL and send the corresponding friend's details
 	const author = req.params.author;
-	return res.send(books[author]);
+	console.log(`looking for author: ${author}`);
+  const booksByAuthor = Object.values(books).filter(book =>
+		book.author.toLowerCase() === author.toLowerCase()
+	);
+
+	return res.json(booksByAuthor);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
 	//Write your code here
-	return res.status(300).json({message: "Yet to be implemented"});
+	const title = req.params.title;
+	console.log(`looking for title: ${title}`);
+	return res.json(
+		Object.values(books).filter(book =>
+			book.title.toLowerCase() === title.toLowerCase()
+		)
+	);
+
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+	//Write your code here
+	return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
